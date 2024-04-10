04e1d0b55793fc492d3c2616cd2c856afe8afe89
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-04-10 17:45:53 +0800

    Create a merge function.

--- mergeSort.js
+++ mergeSort.js
@@ -1,21 +1,4 @@
 /**
- * build a function mergeSort that takes in an array and returns a sorted array,
- * using a recursive merge sort methodology. An input of [3, 2, 1, 13, 8, 5, 0, 1]
- * should return [0, 1, 1, 2, 3, 5, 8, 13]
- * and an input of [105, 79, 100, 110] should return 
- * [79, 100, 105, 110]
- *
- * tips
- * think about what base case is and what behavior is happening
- * again and again and can actually be delegated to someone else
- * (e.g. that same function!).
- *
- * it may be helpful to check out the background videos again if
- * you don't quite understand what should be going on.
- */
-
-
-/**
  * Pseudocode
  * sort half left 
  * sort half right
@@ -25,12 +8,31 @@
 const first = [3, 2, 1, 13, 8, 5, 0, 1]; //  [0, 1, 1, 2, 3, 5, 8, 13]
 const second = [105, 79, 100, 110] // [79, 100, 105, 110]
 
-const mergeSort = function mergeSort(array, left, right) {
-  if (array.length <= 0) {
-    return array;
+const merge = (A, B, C = []) => {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  while (k !== A.length + B.length) {
+
+    if (A[i] > B[j]) {
+      C[k] = B[j];
+      k++;
+      j++;
+    } else {
+      C[k] = A[i];
+      k++;
+      i++;
+    }
+
+    if (A[i] === undefined && B[j] !== undefined) {
+      C[k] = B[j];
+      k++;
+      j++;
+    }
   }
 
-  let mid = array.length / 2;
-  mergeSort(mid, left, right);
-  mergeSort(mid + 1, left, right);
-}
+  return C;
+};
+
+console.log(merge([1], [2],  output = []));
