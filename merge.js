7c9c2d815a9c61a4d9c28ad9415eeba42edba75f
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-04-16 12:48:16 +0800

    Loop remaining item add to the end
    Use this merge function in the mergeSort file to see if it works

--- merge.js
+++ merge.js
@@ -1,4 +1,4 @@
-let fArr = [2, 1, 0, 5, 3, 12, 10, 9]; 
+let first = [0, 2, -5, 3, 10, 12, 9, 11]; 
 
 const merge = (array, start, mid, end) => {
 
@@ -10,6 +10,7 @@ const merge = (array, start, mid, end) => {
   let j = 0;
   let k = 0;
 
+  // add to fArr and sArr
   for (let i = start; i < end; i++) {
     let element = array[i];
 
@@ -24,10 +25,35 @@ const merge = (array, start, mid, end) => {
     }
   }
 
-  return { fArr, sArr }
+  // initialize index array for sorting
+  let l = 0,
+    m = 0,
+    n = 0;
+
+  let arr = [];
+  console.log(fArr, sArr)
+
+  for (let i = 0; i < fArr.length + sArr.length; i++) {
+    if (fArr[l] < sArr[m]) {
+      arr[n++] = fArr[l++];
+    } else {
+      arr[n++] = sArr[m++];
+    }
+  }
+
+  for (; n < fArr.length; n++) {
+    arr[n] = fArr[l++];
+  }
+
+  for (; n < sArr.length; n++) {
+    arr[n] = sArr[m++]
+  }
+
+  array.splice(start, end, ...arr);
+
+  return { array }
 };
 
+const mergin = merge(first, 0, 4, 8);
 
-const mergin = merge(fArr, 4, 6, 8);
-
-console.log(mergin.fArr, mergin.sArr);
+console.log(mergin.array);
